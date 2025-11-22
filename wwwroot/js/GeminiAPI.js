456a46743deba9063e78d564cb49ed85d14ef9b2
AFOOT03939 <victorgvmontero@gmail.com>
2025-11-21 19:21:40 -0600

    diseño

--- wwwroot/js/GeminiAPI.js
+++ wwwroot/js/GeminiAPI.js
@@ -1,4 +1,4 @@
-﻿const GEMINI_API_KEY = "AIzaSyCe7y4gpV7eXTNlRUDK5BfbGAqDOiwNhOM"; 
+﻿const GEMINI_API_KEY = "micadena"; 
 const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 
 async function callGeminiAPI(prompt, isJsonMode = false) {
